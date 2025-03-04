ec73cdb8a8d5a8eca88e659183c4d41b5aafc2c6
amandine <amandine.douguedroit@gmail.com>
2025-03-04 17:14:00 +0200

    Added links between skills and experience/projects

--- script/script.js
+++ script/script.js
@@ -1,43 +1,44 @@
 // Function to add the "navbarDark" class to the navbar on scroll
 function handleNavbarScroll() {
-    const header = document.querySelector(".navbar");
-    window.onscroll = function () {
-        const top = window.scrollY;
-        if (top >= 100) {
-            header.classList.add("navbarDark");
-        } else {
-            header.classList.remove("navbarDark");
-        }
-    };
+  const header = document.querySelector(".navbar");
+  window.onscroll = function () {
+    const top = window.scrollY;
+    if (top >= 100) {
+      header.classList.add("navbarDark");
+    } else {
+      header.classList.remove("navbarDark");
+    }
+  };
 }
 
 // Function to handle navbar collapse on small devices after a click
 function handleNavbarCollapse() {
-    const navLinks = document.querySelectorAll(".nav-item");
-    const menuToggle = document.getElementById("navbarSupportedContent");
+  const navLinks = document.querySelectorAll(".nav-item");
+  const menuToggle = document.getElementById("navbarSupportedContent");
 
-    navLinks.forEach((link) => {
-        link.addEventListener("click", () => {
-            new bootstrap.Collapse(menuToggle).toggle();
-        });
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      new bootstrap.Collapse(menuToggle).toggle();
     });
+  });
 }
 
 // Function to dynamically create HTML elements from the JSON file
 function createSkillsFromJSON() {
-    const container = document.querySelector("#skills .container");
-    let row = document.createElement("div");
-    row.classList.add("row");
+  const container = document.querySelector("#skills .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
 
-    // Load the JSON file
-    fetch("data/skills.json")
-        .then((response) => response.json())
-        .then((data) => {
-            // Iterate through the JSON data and create HTML elements
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                card.innerHTML = `
+  // Load the JSON file
+  fetch("data/skills.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Iterate through the JSON data and create HTML elements
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
+                  <a class="skills-link" href="${item.link}">
                     <div class="card skillsText">
                         <div class="card-body">
                             <img src="./images/${item.image}" alt="${item.alt}"/>
@@ -45,109 +46,119 @@ function createSkillsFromJSON() {
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
+                  </a>
                 `;
 
-                // Append the card to the current row
-                row.appendChild(card);
+        // Append the card to the current row
+        row.appendChild(card);
 
-                // If the index is a multiple of 3 or it's the last element, create a new row
-                if ((index + 1) % 3 === 0 || index === data.length - 1) {
-                    container.appendChild(row);
-                    row = document.createElement("div");
-                    row.classList.add("row");
-                }
-            });
-        });
+        // If the index is a multiple of 3 or it's the last element, create a new row
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
 }
 
 // Function to dynamically create HTML elements from the JSON file
 function createPortfolioFromJSON() {
-    const container = document.querySelector("#portfolio .container");
-    let row = document.createElement("div");
-    row.classList.add("row");
+  const container = document.querySelector("#portfolio .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
 
-    // Load the JSON file
-    fetch("data/portfolio.json")
-        .then((response) => response.json())
-        .then((data) => {
-            // Iterate through the JSON data and create HTML elements
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                let html = item.text;
-                if (html.includes("\n")) html = html.replace(/\n/g, "<br>");
-                card.innerHTML = `
-                    <div class="card portfolioContent">
+  // Load the JSON file
+  fetch("data/portfolio.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Iterate through the JSON data and create HTML elements
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        let html = item.text;
+        if (html.includes("\n")) html = html.replace(/\n/g, "<br>");
+        card.innerHTML = `
+                    <div id=${item.id} class="card portfolioContent">
                     <img class="card-img-top" src="images/${item.image}" style="width:100%" alt="${item.alt}">
                     <div class="card-body">
                         <h3 class="card-title">${item.title}</h3>
                         <p class="card-text">${html}</p>
-                        <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
+                        <div class="div-project-link text-center">
+                            <a href="${item.link}" class="project-link btn btn-success">Lien</a>
                         </div>
                     </div>
                 </div>
                 `;
 
-                // Append the card to the current row
-                row.appendChild(card);
+        // Append the card to the current row
+        row.appendChild(card);
 
-                // If the index is a multiple of 3 or it's the last element, create a new row
-                if ((index + 1) % 3 === 0 || index === data.length - 1) {
-                    container.appendChild(row);
-                    row = document.createElement("div");
-                    row.classList.add("row");
-                }
-            });
-        });
+        // If the index is a multiple of 3 or it's the last element, create a new row
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+
+      // Remove link when empty
+      let cards = document.getElementsByClassName("card portfolioContent");
+      for (let card of cards) {
+        const cardBody = card.children[1];
+        const divProjectLink = cardBody.children[2];
+        const projectLink = divProjectLink.children[0];
+        if (projectLink.getAttribute("href") === "#") cardBody.removeChild(divProjectLink);
+      }
+    });
 }
 
 // Function to dynamically create HTML elements from the JSON file
 function createExperienceFromJSON() {
-    const container = document.querySelector("#experience .container");
-    let row = document.createElement("div");
-    row.classList.add("row", "row-center");
+  const container = document.querySelector("#experience .container");
+  let row = document.createElement("div");
+  row.classList.add("row", "row-center");
 
-    // Load the JSON file
-    fetch("data/experience.json")
-        .then((response) => response.json())
-        .then((data) => {
-            // Iterate through the JSON data and create HTML elements
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                let html = item.text;
-                if (html.includes("\n")) html = html.replace(/\n/g, "<br>");
-                card.innerHTML = `
-                    <div class="card experienceContent">
+  // Load the JSON file
+  fetch("data/experience.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Iterate through the JSON data and create HTML elements
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        let html = item.text;
+        if (html.includes("\n")) html = html.replace(/\n/g, "<br>");
+        card.innerHTML = `
+                    <div id="${item.id}" class="card experienceContent">
                     <img class="card-img-top" src="images/${item.image}" style="width:100%" alt="${item.alt}">
                     <div class="card-body">
                         <h3 class="card-title">${item.title}</h3>
                         <h4 class="card-subtitle">${item.subtitle}</h4>
                         <p class="card-text">${html}</p>
                         <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
+                            <a href="${item.link}" class="project-link btn btn-success" target="_blank">Lien</a>
                         </div>
                     </div>
                 </div>
                 `;
 
-                // Append the card to the current row
-                row.appendChild(card);
+        // Append the card to the current row
+        row.appendChild(card);
 
-                // If the index is a multiple of 3 or it's the last element, create a new row
-                if ((index + 1) % 3 === 0 || index === data.length - 1) {
-                    container.appendChild(row);
-                    row = document.createElement("div");
-                    row.classList.add("row");
-                }
-            });
-        });
+        // If the index is a multiple of 3 or it's the last element, create a new row
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
 }
 
 // Call the functions to execute the code
 handleNavbarScroll();
 handleNavbarCollapse();
 createSkillsFromJSON();
-createExperienceFromJSON()
+createExperienceFromJSON();
 createPortfolioFromJSON();
